19b5e8519dfb8f6363d9100e2630a818793996f6
avelarai <avelarai269@gmail.com>
2026-07-14 23:32:45 +0200

    aselar docs

--- aselar-receipts/src/Utils/Banner.tsx
+++ aselar-receipts/src/Utils/Banner.tsx
@@ -94,11 +94,15 @@ const Banner = ({ isCollapsed }: BannerProps) => {
 
         <div className={bannerStyles.actionSection}>
           <div className={bannerStyles.notifications}>
+            <a href="https://aselar.netlify.app/" className={bannerStyles.iconButton} title="Aselar Documentation">
+              <FaFile color="yellow" size={24}/>
+            </a>             
+          </div>
+<div className={bannerStyles.notifications}>
             <Link to="/files-bunker" className={bannerStyles.iconButton} title="Go to Files Bunker">
               <FaFile color="blue" size={24}/>
             </Link>             
           </div>
-
           
 
           <div className={`${bannerStyles.calculatorShow} ${bannerStyles.dropdownContainer}`}>
